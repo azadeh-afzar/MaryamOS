554d330edf444a6b8fe18717e810f54e8f936bae
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-07-19 00:45:04 +0430

    add: cursor theme compile script

--- render-assets.sh
+++ render-assets.sh
@@ -4,6 +4,7 @@ OPTIPNG="/usr/bin/optipng"
 
 REPO_DIR=$(cd $(dirname $0) && pwd)
 ASRC_DIR="${REPO_DIR}/src/assets"
+CURSOR_DIR="${REPO_DIR}/cursors"
 
 function inkscape() {
   command flatpak run org.inkscape.Inkscape "$@"
@@ -35,5 +36,9 @@ done
 echo "Rendering gtk-3.0 assets"
 cd "$ASRC_DIR/gtk-3.0/common-assets" && ./render-assets.sh
 cd "$ASRC_DIR/gtk-3.0/windows-assets" && ./render-assets.sh && ./render-alt-assets.sh && ./render-small-assets.sh && ./render-alt-small-assets.sh
+
+echo "Rendering Cursor assets"
+cd "$CURSOR_DIR" && ./build.sh
+
 
 exit 0
