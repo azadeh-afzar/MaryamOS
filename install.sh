febc17360ea522a3668a7094fa6b278dc7e615b7
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-07-31 22:50:11 +0430

    modify: colored info in install script

--- install.sh
+++ install.sh
@@ -26,6 +26,47 @@ OPACITY_VARIANTS=('' '-solid')
 ALT_VARIANTS=('' '-alt')
 ICON_VARIANTS=('' '-normal' '-gnome' '-ubuntu' '-arch' '-manjaro' '-fedora' '-debian' '-void')
 
+
+#COLORS
+CDEF=" \033[0m"                                     # default color
+CCIN=" \033[0;36m"                                  # info color
+CGSC=" \033[0;32m"                                  # success color
+CRER=" \033[0;31m"                                  # error color
+CWAR=" \033[0;33m"                                  # waring color
+b_CDEF=" \033[1;37m"                                # bold default color
+b_CCIN=" \033[1;36m"                                # bold info color
+b_CGSC=" \033[1;32m"                                # bold success color
+b_CRER=" \033[1;31m"                                # bold error color
+b_CWAR=" \033[1;33m"                                # bold warning color
+
+# echo like ...  with  flag type  and display message  colors
+prompt () {
+  case ${1} in
+    "-s"|"--success")
+      echo -e "${b_CGSC}${@:2}${CDEF}";;    # print success message
+    "-e"|"--error")
+      echo -e "${b_CRER}${@:2}${CDEF}";;    # print error message
+    "-w"|"--warning")
+      echo -e "${b_CWAR}${@:2}${CDEF}";;    # print warning message
+    "-i"|"--info")
+      echo -e "${b_CCIN}${@:2}${CDEF}";;    # print info message
+    *)
+    echo -e "$@"
+    ;;
+  esac
+}
+
+# Check command avalibility
+function has_command() {
+  command -v $1 > /dev/null
+}
+
+operation_canceled() {
+  clear
+  prompt  -i "\n Operation canceled by user, Bye!"
+  exit 1
+}
+
 usage() {
   printf "%s\n" "Usage: $0 [OPTIONS...]"
   printf "\n%s\n" "OPTIONS:"
@@ -34,7 +75,6 @@ usage() {
   printf "  %-25s%s\n" "-o, --opacity VARIANTS" "Specify theme opacity variant(s) [standard|solid] (Default: All variants)"
   printf "  %-25s%s\n" "-c, --color VARIANTS" "Specify theme color variant(s) [light|dark] (Default: All variants)"
   printf "  %-25s%s\n" "-a, --alt VARIANTS" "Specify theme titilebutton variant(s) [standard|alt] (Default: All variants)"
-  printf "  %-25s%s\n" "-s, --small VARIANTS" "Specify titilebutton size variant(s) [standard|small] (Default: standard variant)"
   printf "  %-25s%s\n" "-i, --icon VARIANTS" "Specify activities icon variant(s) for gnome-shell [standard|normal|gnome|ubuntu|arch|manjaro|fedora|debian|void] (Default: standard variant)"
   printf "  %-25s%s\n"     "--snap"          "Install modifed Snap application .desktop files to apply custom theme."
   printf "  %-25s%s\n" "-g, --gdm" "Install GDM theme, this option need root user authority! please run this with sudo"
@@ -62,7 +102,7 @@ install() {
 
   [[ -d ${THEME_DIR} ]] && rm --recursive --force ${THEME_DIR}
 
-  echo "Installing '${THEME_DIR}'..."
+  prompt --info "Installing '${THEME_DIR}'..."
 
   mkdir --parents                                                                                          "${THEME_DIR}"
   cp --update --recursive "${REPO_DIR}/LICENSE"                                                            "${THEME_DIR}"
@@ -84,8 +124,10 @@ install() {
   # set right icon for activity panel
   local var="\$icon-logo: '${icon}';";
   sed --in-place "1s/.*/${var}/"  "${SRC_DIR}/main/gnome-shell/gnome-shell${color}${opacity}.scss"
+  
   # generate css files.
   parse_sass
+  
   # copy css files.
   mkdir --parents                                                                                          "${THEME_DIR}/gnome-shell"
   cp --update --recursive "${SRC_DIR}/assets/gnome-shell/source-assets"/*                                  "${THEME_DIR}/gnome-shell"
@@ -97,6 +139,7 @@ install() {
   # install gtk3 theme.
   mkdir --parents                                                                                          "${THEME_DIR}/gtk-3.0"
   cp --update --recursive "${SRC_DIR}/assets/gtk-3.0/common-assets/assets"                                 "${THEME_DIR}/gtk-3.0"
+  cp --update --recursive "${SRC_DIR}/assets/gtk-3.0/common-assets/sidebar-assets"/*.png                   "${THEME_DIR}/gtk-3.0/assets"
   cp --update --recursive "${SRC_DIR}/assets/gtk-3.0/windows-assets/titlebutton${alt}"                     "${THEME_DIR}/gtk-3.0/windows-assets"
   cp --update --recursive "${SRC_DIR}/assets/gtk-3.0/thumbnail${color}.png"                                "${THEME_DIR}/gtk-3.0/thumbnail.png"
   cp --update --recursive "${SRC_DIR}/main/gtk-3.0/gtk-dark${opacity}.css"                                 "${THEME_DIR}/gtk-3.0/gtk-dark.css"
@@ -133,10 +176,10 @@ install_gdm() {
   local GDM_THEME_DIR="${1}/${2}${3}${4}"
 
   echo
-  echo "Installing ${2}${3}${4} gdm theme..."
+  prompt --info "Installing ${2}${3}${4} gdm theme..."
 
   if [[ -f "$GS_THEME_FILE" ]] && command -v glib-compile-resources >/dev/null ; then
-    echo "Installing '$GS_THEME_FILE'..."
+    prompt --info "Installing '$GS_THEME_FILE'..."
     cp --archive --no-clobber "$GS_THEME_FILE" "$GS_THEME_FILE.bak"
     glib-compile-resources \
       --sourcedir="$GDM_THEME_DIR/gnome-shell" \
@@ -145,19 +188,19 @@ install_gdm() {
   fi
 
   if [[ -f "$UBUNTU_THEME_FILE" && -f "$GS_THEME_FILE.bak" ]]; then
-    echo "Installing '$UBUNTU_THEME_FILE'..."
+    prompt --info "Installing '$UBUNTU_THEME_FILE'..."
     cp --archive --no-clobber "$UBUNTU_THEME_FILE" "$UBUNTU_THEME_FILE.bak"
     cp --archive --force "$GDM_THEME_DIR/gnome-shell/gnome-shell.css" "$UBUNTU_THEME_FILE"
   fi
 
   if [[ -f "$UBUNTU_NEW_THEME_FILE" && -f "$GS_THEME_FILE.bak" ]]; then
-    echo "Installing '$UBUNTU_NEW_THEME_FILE'..."
+    prompt --info "Installing '$UBUNTU_NEW_THEME_FILE'..."
     cp --archive --no-clobber "$UBUNTU_NEW_THEME_FILE" "$UBUNTU_NEW_THEME_FILE.bak"
     cp --archive --force "$GDM_THEME_DIR"/gnome-shell/* "$SHELL_THEME_FOLDER"
   fi
 
   if [[ -f "$ETC_THEME_FILE" && -f "$GS_THEME_FILE.bak" ]]; then
-    echo "Installing Ubuntu gnome-shell theme..."
+    prompt --info "Installing Ubuntu gnome-shell theme..."
     cp --archive --no-clobber "$ETC_THEME_FILE" "$ETC_THEME_FILE.bak"
     [[ -d $SHELL_THEME_FOLDER/$THEME_NAME ]] && rm --recursive --force $SHELL_THEME_FOLDER/$THEME_NAME
     cp --update --recursive "$GDM_THEME_DIR/gnome-shell" "$SHELL_THEME_FOLDER/$THEME_NAME"
@@ -168,37 +211,90 @@ install_gdm() {
 
 revert_gdm() {
   if [[ -f "$GS_THEME_FILE.bak" ]]; then
-    echo "reverting '$GS_THEME_FILE'..."
+    prompt --warning "reverting '$GS_THEME_FILE'..."
     rm --recursive --force "$GS_THEME_FILE"
     mv "$GS_THEME_FILE.bak" "$GS_THEME_FILE"
   fi
 
   if [[ -f "$UBUNTU_THEME_FILE.bak" ]]; then
-    echo "reverting '$UBUNTU_THEME_FILE'..."
+    prompt --warning "reverting '$UBUNTU_THEME_FILE'..."
     rm --recursive --force "$UBUNTU_THEME_FILE"
     mv "$UBUNTU_THEME_FILE.bak" "$UBUNTU_THEME_FILE"
   fi
 
   if [[ -f "$UBUNTU_NEW_THEME_FILE.bak" ]]; then
-    echo "reverting '$UBUNTU_NEW_THEME_FILE'..."
+    prompt --warning "reverting '$UBUNTU_NEW_THEME_FILE'..."
     rm --recursive --force "$UBUNTU_NEW_THEME_FILE" "$SHELL_THEME_FOLDER"/{assets,no-events.svg,process-working.svg,no-notifications.svg}
     mv "$UBUNTU_NEW_THEME_FILE.bak" "$UBUNTU_NEW_THEME_FILE"
   fi
 
   if [[ -f "$ETC_THEME_FILE.bak" ]]; then
-    echo "reverting Ubuntu gnome-shell theme..."
+    prompt --warning "reverting Ubuntu gnome-shell theme..."
     rm --recursive --force "$ETC_THEME_FILE"
     mv "$ETC_THEME_FILE.bak" "$ETC_THEME_FILE"
     [[ -d $SHELL_THEME_FOLDER/$THEME_NAME ]] && rm --recursive --force $SHELL_THEME_FOLDER/$THEME_NAME
   fi
 }
 
+
+install_dialog() {
+  if [ ! "$(which dialog 2> /dev/null)" ]; then
+    prompt -w "\n 'dialog' needs to be installed for this shell"
+    if has_command zypper; then
+      sudo zypper in dialog
+    elif has_command apt-get; then
+      sudo apt-get install dialog
+    elif has_command dnf; then
+      sudo dnf install -y dialog
+    elif has_command yum; then
+      sudo yum install dialog
+    elif has_command pacman; then
+      sudo pacman -S --noconfirm dialog
+    fi
+  fi
+}
+
+run_dialog() {
+  if [[ -x /usr/bin/dialog ]]; then
+    tui=$(dialog --backtitle "${THEME_NAME} gtk theme installer" \
+    --radiolist "Choose your nautilus sidebar size (default is 180px width): " 15 40 5 \
+      1 "180px" on  \
+      2 "200px" off \
+      3 "220px" off  \
+      4 "240px" off  \
+      5 "260px" off --output-fd 1 )
+      case "$tui" in
+        1) sidebar_size="180px" ;;
+        2) sidebar_size="200px" ;;
+        3) sidebar_size="220px" ;;
+        4) sidebar_size="240px" ;;
+        5) sidebar_size="260px" ;;
+        *) operation_canceled ;;
+     esac
+  fi
+}
+
+change_size() {
+  cd "${SRC_DIR}/sass/gtk"
+  cp --archive --no-clobber _applications.scss _applications.scss.bak
+  sed --in-place "s/180px/$sidebar_size/g" _applications.scss
+  prompt --warning "Change nautilus sidebar size ..."
+}
+
+restore_file() {
+  cd ${SRC_DIR}/sass/gtk
+  [[ -f _applications.scss.bak ]] && rm --recursive --force _applications.scss
+  mv _applications.scss.bak _applications.scss
+  prompt --warning "Restore scss file ..."
+}
+
+
 while [[ $# -gt 0 ]]; do
   case "${1}" in
     -d|--dest)
       dest="${2}"
       if [[ ! -d "${dest}" ]]; then
-        echo "Destination directory does not exist. Let's make a new one..."
+        prompt --info "Destination directory does not exist. Let's make a new one..."
         mkdir --parents ${dest}
       fi
       shift 2
@@ -213,6 +309,10 @@ while [[ $# -gt 0 ]]; do
       ;;
     -g|--gdm)
       gdm='true'
+      shift 1
+      ;;
+    -s|--size)
+      size='true'
       shift 1
       ;;
     -r|--revert)
@@ -235,8 +335,8 @@ while [[ $# -gt 0 ]]; do
             break
             ;;
           *)
-            echo "ERROR: Unrecognized opacity variant '$1'."
-            echo "Try '$0 --help' for more information."
+            prompt --error "ERROR: Unrecognized opacity variant '$1'."
+            prompt --info "Try '$0 --help' for more information."
             exit 1
             ;;
         esac
@@ -258,8 +358,8 @@ while [[ $# -gt 0 ]]; do
             break
             ;;
           *)
-            echo "ERROR: Unrecognized opacity variant '$1'."
-            echo "Try '$0 --help' for more information."
+            prompt --error "ERROR: Unrecognized opacity variant '$1'."
+            prompt --info "Try '$0 --help' for more information."
             exit 1
             ;;
         esac
@@ -281,8 +381,8 @@ while [[ $# -gt 0 ]]; do
             break
             ;;
           *)
-            echo "ERROR: Unrecognized color variant '$1'."
-            echo "Try '$0 --help' for more information."
+            prompt --error "ERROR: Unrecognized color variant '$1'."
+            prompt --info "Try '$0 --help' for more information."
             exit 1
             ;;
         esac
@@ -332,8 +432,8 @@ while [[ $# -gt 0 ]]; do
             break
             ;;
           *)
-            echo "ERROR: Unrecognized icon variant '$1'."
-            echo "Try '$0 --help' for more information."
+            prompt --error "ERROR: Unrecognized icon variant '$1'."
+            prompt --info "Try '$0 --help' for more information."
             exit 1
             ;;
         esac
@@ -344,13 +444,23 @@ while [[ $# -gt 0 ]]; do
       exit 0
       ;;
     *)
-      echo "ERROR: Unrecognized installation option '$1'."
-      echo "Try '$0 --help' for more information."
+      prompt --error "ERROR: Unrecognized installation option '$1'."
+      prompt --info "Try '$0 --help' for more information."
       exit 1
       ;;
   esac
 done
 
+# Change sidebar width if specified by user.
+if [[ "${size:-}" == 'true' ]]; then
+  install_dialog && run_dialog
+
+  if [[ "$sidebar_size" != '180px' ]]; then
+    change_size
+  fi
+fi
+
+# install theme
 install_theme() {
 for opacity in "${opacities[@]-${OPACITY_VARIANTS[@]}}"; do
   for color in "${colors[@]-${COLOR_VARIANTS[@]}}"; do
@@ -375,16 +485,21 @@ if [[ "${gdm:-}" != 'true' && "${revert:-}" == 'true' && "$UID" -eq "$ROOT_UID" 
   revert_gdm
 fi
 
+# Revert size change.
+if [[ -f "${SRC_DIR}"/sass/gtk/_applications.scss.bak ]]; then
+  restore_file && parse_sass
+fi
+
 # copy snap desktop files.
 if [[ "${snap:-}" == 'true' ]]; then
   echo
-  echo "Install custome snap pakage icons..."
-  cp -r "${REPO_DIR}/icons/snap"/*.desktop "${APP_DIR}"
+  prompt --info "Install custome snap pakage icons..."
+  cp --recursive "${REPO_DIR}/icons/snap"/*.desktop "${APP_DIR}"
 fi
 
 # Install cursors
 echo
-echo "Installing cursors..."
+prompt --info "Installing cursors..."
 if [ "$UID" -eq "$ROOT_UID" ]; then
   CURSOR_DEST_DIR="/usr/share/icons"
 else
@@ -397,16 +512,16 @@ fi
 
 cd "${REPO_DIR}/cursors"
 cp --recursive dist "$CURSOR_DEST_DIR/${THEME_NAME}-Cursors"
-echo "Installing cursors ... DONE"
+prompt --success "Installing cursors ... DONE"
 
 # Install Icons
 echo
-echo "Installing icons..."
+prompt --info "Installing icons..."
 
 cd "${REPO_DIR}/icons"
 ./install.sh
 
-echo "Installing icons ... DONE"
+prompt --success "Installing icons ... DONE"
 
 echo
-echo "Done."
+prompt --success "Done."
