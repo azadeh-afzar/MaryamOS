a8b122c7142b8c522aa8cc83111baa7354ef9a2a
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-08-04 18:57:08 +0430

    refactor: cleanup unused files

--- icons/circle/install.sh
+++ icons/circle/install.sh
@@ -13,7 +13,6 @@ fi
 SRC_DIR="$(cd $(dirname $0) && pwd)"
 
 THEME_NAME="MaryamOS-Icons-Circle"
-THEME_VARIANTS=('' '-red' '-pink' '-purple' '-blue' '-green' '-yellow' '-orange' '-brown' '-grey' '-black')
 COLOR_VARIANTS=('' '-dark')
 
 usage() {
@@ -21,18 +20,6 @@ usage() {
   printf "\n%s\n" "OPTIONS:"
   printf "  %-25s%s\n" "-d, --dest DIR" "Specify theme destination directory (Default: ${DEST_DIR})"
   printf "  %-25s%s\n" "-n, --name NAME" "Specify theme name (Default: ${THEME_NAME})"
-  printf "  %-25s%s\n" "-c, --circle" "Install circle folder version"
-  printf "  %-25s%s\n" "-w, --white" "Install white panel icon color version"
-  printf "  %-25s%s\n" "-a, --all" "Install all color folder versions"
-  printf "  %-25s%s\n" "-red" "Red color folder version"
-  printf "  %-25s%s\n" "-pink" "Pink color folder version"
-  printf "  %-25s%s\n" "-purple" "Purple color folder version"
-  printf "  %-25s%s\n" "-blue" "Blue color folder version"
-  printf "  %-25s%s\n" "-green" "Green color folder version"
-  printf "  %-25s%s\n" "-yellow" "Yellow color folder version"
-  printf "  %-25s%s\n" "-orange" "Orange color folder version"
-  printf "  %-25s%s\n" "-brown" "Brown color folder version"
-  printf "  %-25s%s\n" "-black" "Black color folder version"
   printf "  %-25s%s\n" "-h, --help" "Show this help"
 }
 
@@ -50,14 +37,6 @@ install() {
   mkdir --parents                                                                                     "${THEME_DIR}"
   cp --recursive "${SRC_DIR}/src/index.theme"                                                         "${THEME_DIR}"
 
-  if [[ "${DESKTOP_SESSION}" == '/usr/share/xsessions/plasma' && "${color}" == '' ]]; then
-    sed --in-place "s/Adwaita/breeze/g" "${THEME_DIR}/index.theme"
-  fi
-
-  if [[ "${DESKTOP_SESSION}" == '/usr/share/xsessions/plasma' && "${color}" == '-dark' ]]; then
-    sed --in-place "s/Adwaita/breeze-dark/g" "${THEME_DIR}/index.theme"
-  fi
-
   cd "${THEME_DIR}"
   sed --in-place "s/${name}/${name}${theme}${color}/g" "index.theme"
 
@@ -65,24 +44,7 @@ install() {
     mkdir --parents                                                                                   "${THEME_DIR}/status"
     cp --recursive "${SRC_DIR}/src"/{actions,animations,apps,categories,devices,emblems,mimes,places} "${THEME_DIR}"
     cp --recursive "${SRC_DIR}/src/status"/{16,22,24,32,symbolic}                                     "${THEME_DIR}/status"
-
-    if [[ "${white}" == 'true' ]]; then
-      sed --in-place "s/#363636/#dedede/g" "${THEME_DIR}/status"/{16,22,24}/*
-    fi
-
     cp --recursive "${SRC_DIR}/links"/{actions,apps,categories,devices,emblems,mimes,places,status}   "${THEME_DIR}"
-  fi
-
-  if [[ "${color}" == '' && "${theme}" != '' ]]; then
-    cp --recursive "${SRC_DIR}/colorful-folder/folder${theme}"/*.svg                                  "${THEME_DIR}/places/48"
-  fi
-
-  if [[ "${circle}" == 'true' && "${theme}" == '' && "${color}" == '' ]]; then
-    cp --recursive "${SRC_DIR}/circle-folder"/*.svg                                                   "${THEME_DIR}/places/48"
-  fi
-
-  if [[ "${color}" == '' && "${DESKTOP_SESSION}" == '/usr/share/xsessions/budgie-desktop' ]]; then
-    cp --recursive "${SRC_DIR}/src/status/symbolic-budgie"/*.svg                                      "${THEME_DIR}/status/symbolic"
   fi
 
   if [[ "${color}" == '-dark' ]]; then
@@ -155,45 +117,6 @@ while [[ $# -gt 0 ]]; do
       name="${2}"
       shift 2
       ;;
-    -c|--circle)
-      circle='true'
-      ;;
-    -a|--all)
-      all="true"
-      ;;
-    -w|--white)
-      white="true"
-      ;;
-    -black)
-      theme="-black"
-      ;;
-    -blue)
-      theme="-blue"
-      ;;
-    -brown)
-      theme="-brown"
-      ;;
-    -green)
-      theme="-green"
-      ;;
-    -grey)
-      theme="-grey"
-      ;;
-    -orange)
-      theme="-orange"
-      ;;
-    -pink)
-      theme="-pink"
-      ;;
-    -purple)
-      theme="-purple"
-      ;;
-    -red)
-      theme="-red"
-      ;;
-    -yellow)
-      theme="-yellow"
-      ;;
     -h|--help)
       usage
       exit 0
@@ -213,16 +136,4 @@ install_theme() {
   done
 }
 
-install_all() {
-for theme in "${themes[@]-${THEME_VARIANTS[@]}}"; do
-  for color in "${colors[@]-${COLOR_VARIANTS[@]}}"; do
-    install "${dest:-${DEST_DIR}}" "${name:-${THEME_NAME}}" "${color}"
-  done
-done
-}
-
-if [[ "${all}" == 'true' ]]; then
-  install_all
-  else
-  install_theme
-fi
+install_theme
