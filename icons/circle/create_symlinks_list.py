e27db58d36bd4ca53ce1b26ee6d0997b9c2669a0
Mohammad Mahdi Baghbani Pourvahid <MahdiBaghbani@protonmail.com>
2020-08-05 10:50:30 +0430

    modify: make code more readable

--- icons/circle/create_symlinks_list.py
+++ icons/circle/create_symlinks_list.py
@@ -15,21 +15,21 @@ dont_use = "links"
 categories = ["actions", "apps", "devices", "emblems", "mimes", "places", "status"]
 
 for dir in dirs:
-    dir_name = dir.split("/")[-1]
-    upper_dirname = dir.split("/")[-2]
+    dir_name = os.path.basename(dir)
+    upper_dirname = os.path.basename(os.path.dirname(dir))
 
     if dir_name == dont_use:
         continue
 
     if dir_name in categories:
-        mkpath = os.path.join(file_path, LINKS_LIST_DIR, dir_name)
-        subprocess.call(f"mkdir -p \"{mkpath}\"", shell=True)
+        category_dir = os.path.join(file_path, LINKS_LIST_DIR, dir_name)
+        subprocess.call(f"mkdir -p \"{category_dir}\"", shell=True)
         continue
 
-    lspath = os.path.join(mkpath, f"{dir_name}.list")
-    subprocess.call(f"ls \"{dir}\" -lhaF | grep ^l | grep -v \"\#\" | cut -c62- | sed 's/ ->//g' > \"{lspath}\"", shell=True)
+    list_path = os.path.join(category_dir, f"{dir_name}.list")
+    subprocess.call(f"ls \"{dir}\" -lhaF | grep ^l | grep -v \"\#\" | cut -c62- | sed 's/ ->//g' > \"{list_path}\"", shell=True)
     
-    with open(f"{lspath}", "r") as file:
+    with open(f"{list_path}", "r") as file:
         content = file.readlines()
     
     for index, line in enumerate(content):
@@ -38,5 +38,5 @@ for dir in dirs:
         line = " ".join(line)
         content[index] = line
 
-    with open(f"{lspath}", "w+") as file:
+    with open(f"{list_path}", "w+") as file:
         file.writelines(sorted(content))
